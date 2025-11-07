4c66710910e251eb2ebfa77f456f9c96b9fd86cd
Saber1Y <ayodejiakintobi1@gmail.com>
2025-11-07 08:14:51 +0100

    Remove PredictionMarket and PredictionMarketFactory contracts; clean up unused files

---
+++ backend/src/core/PreditionMarketFactory.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+contract PredictionMarketFactory {
+    struct Market {
+        uint256 id;
+        string question;
+        string category;
+        bool outcome;
+        string imageUrl;
+        address creator;
+        address liquidity;
+        status activeStatus;
+        uint256 totalShares;
+        uint256 yeshare;
+        uint256 noShare
+    }
+
+    mapping(address => bool) public admins;
+    address[] public markets;
+
+    mapping(uint256 => address) public marketById;
+    uint256 public marketCount;
+
+    modifier onlyAdmin() {
+        require(admins[msg.sender], "Only admin can perform this action");
+        _;
+    }
+
+    function createMarket(uint256 id, string memory question, string memory imageUrl) public payable onlyAdmin {
+        PredictionMarket newMarket = new PredictionMarket(id, question, imageUrl, msg.sender);
+        markets.push(address(newMarket));
+        marketById[id] = address(newMarket);
+        marketCount++;
+        emit MarketCreated(id, address(newMarket), question, msg.sender);
+    }
+}
